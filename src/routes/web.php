3151a4ab6b95e1923278d00846306da007b14741
mai <maicranberry669@gmail.com>
2024-10-21 21:32:27 +0900

    ルーティングの作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CommentController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +20,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [ItemController::class, 'index']);
+Route::get('/items/search', [ItemController::class, 'search']);
+Route::get('/item/{item_id}', [ItemController::class, 'detail']);
+
+Route::middleware('auth:web')->group(function () {
+    Route::get('/sell', [ItemController::class, 'add']);
+    Route::post('/sell', [ItemController::class, 'store']);
+    Route::get('/purchase/{item_id}', [PurchaseController::class, 'index']);
+    Route::post('/purchase/{item_id}', [PurchaseController::class, 'store']);
+    Route::post('/like', [LikeController::class, 'store']);
+    Route::post('/unlike', [LikeController::class, 'destroy']);
+    // Route::post('/purchase/address/{item_id}', [ReservationController::class, 'store']);
+    Route::get('/mypage', [UserController::class, 'index']);
+    Route::get('/mypage/profile', [ProfileController::class, 'index']);
+    Route::post('/mypage/profile', [ProfileController::class, 'store']);
+    Route::get('/comment', [CommentController::class, 'index']);
+    Route::post('/comment', [CommentController::class, 'store']);
 });
